7e17cffcf0245faf1be29e4aa0ece0a608c190a6
MastWex <09dracon@gmail.com>
2024-02-21 22:14:46 +0300

    Week2Tasks

---
+++ Assets/Task1/Middle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Middle : MonoBehaviour
+{
+    public Transform PointA;
+    public Transform PointB;
+    public float K;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //        transform.position = (PointA.position + PointB.position) / 2;
+        transform.position = Vector3.Lerp(PointA.position, PointB.position, K);
+    }
+}
---
+++ Assets/Task2/Transformation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transformation : MonoBehaviour
+{
+    public Vector3 Pos1;
+    public Vector3 Pos2;
+    public Color Color1;
+    public Color Color2;
+    public Vector3 Scale1;
+    public Vector3 Scale2;
+    [Range(0f, 1f)]
+    public float value;
+
+    private MeshRenderer _meshRenderer;
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(Pos1, Pos2, value);
+        _meshRenderer.material.color = Color.Lerp(Color1, Color2, value);
+        transform.localScale = Vector3.Lerp(Scale1, Scale2, value);
+    }
+}
